56af3d7af7ffa43257b774c5219537d40fdc0751
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-02 18:23:43 +0000

    initial commit

---
+++ app/src/main/java/com/syzible/rentapp/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.syzible.rentapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ed on 29/05/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference_screen);
+    }
+}
