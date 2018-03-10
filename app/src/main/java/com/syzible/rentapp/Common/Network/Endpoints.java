418c438293397cdfc9d4fa2c52127439c37137b9
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-10 17:30:46 +0000

    added endpoints and a basic rest client

--- app/src/main/java/com/syzible/rentapp/Common/Network/Endpoints.java
+++ app/src/main/java/com/syzible/rentapp/Common/Network/Endpoints.java
@@ -1,4 +1,10 @@
 package com.syzible.rentapp.Common.Network;
 
 public class Endpoints {
+    private static final String HOST_URL = "https://jingly-development.herokuapp.com/api/v1";
+
+    public static final String USER = HOST_URL + "/user";
+    public static final String LISTING = HOST_URL + "/listing";
+    public static final String LANDLORD = HOST_URL + "/landlord";
+    public static final String APPLICATION = HOST_URL + "/listing";
 }
--- app/src/main/java/com/syzible/rentapp/Common/Network/RestClient.java
+++ app/src/main/java/com/syzible/rentapp/Common/Network/RestClient.java
@@ -1,4 +1,30 @@
 package com.syzible.rentapp.Common.Network;
 
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
+
 public class RestClient {
+    public static void get(String url, AsyncHttpResponseHandler responseHandler) {
+        new AsyncHttpClient().get(url, null, responseHandler);
+    }
+
+    public static void post(Context context, String url, JSONObject data, AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
+        new AsyncHttpClient().post(context, url, new StringEntity(data.toString()), "application/json", responseHandler);
+    }
+
+    public static void put(Context context, String url, JSONObject data, AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
+        new AsyncHttpClient().put(context, url, new StringEntity(data.toString()), "application/json", responseHandler);
+    }
+
+    public static void delete(String url, AsyncHttpResponseHandler responseHandler) {
+        new AsyncHttpClient().delete(url, null, responseHandler);
+    }
 }
