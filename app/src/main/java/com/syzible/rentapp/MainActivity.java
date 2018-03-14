af0abfd407b782f9ace287e59ba484c908e7fa47
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-14 22:27:05 +0000

    updated main activity to have fragment transactions

--- app/src/main/java/com/syzible/rentapp/MainActivity.java
+++ app/src/main/java/com/syzible/rentapp/MainActivity.java
@@ -2,11 +2,9 @@ package com.syzible.rentapp;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,7 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.syzible.rentapp.FindProperty.FindPropertyFragment;
+import com.syzible.rentapp.FindProperty.Listing.ViewListingFragment;
+import com.syzible.rentapp.FindProperty.Results.FindPropertyFragment;
 import com.syzible.rentapp.Settings.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -39,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
 
-        setFragment(getFragmentManager(), new FindPropertyFragment());
+        setFragment(getFragmentManager(), new ViewListingFragment());
     }
 
     @Override
