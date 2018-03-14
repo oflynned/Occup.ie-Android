8f99e25c742d215d392c2b1da8cbc82a0e4e1c02
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-14 22:27:58 +0000

    added findproperty listing result fragment

--- app/src/main/java/com/syzible/rentapp/FindProperty/Listing/ViewListingFragment.java
+++ app/src/main/java/com/syzible/rentapp/FindProperty/Listing/ViewListingFragment.java
@@ -1,4 +1,26 @@
 package com.syzible.rentapp.FindProperty.Listing;
 
-public class ViewListingFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.syzible.rentapp.R;
+
+public class ViewListingFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return view = inflater.inflate(R.layout.fragment_listing, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
