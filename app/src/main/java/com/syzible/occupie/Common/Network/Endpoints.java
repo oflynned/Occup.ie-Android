2800a56923f6f186cc6699c8f660848b8b220b6a
Edmond O' Flynn <oflynned@tcd.ie>
2018-03-25 00:59:02 +0000

    updated endpoints

--- app/src/main/java/com/syzible/occupie/Common/Network/Endpoints.java
+++ app/src/main/java/com/syzible/occupie/Common/Network/Endpoints.java
@@ -4,7 +4,8 @@ public class Endpoints {
     private static final String HOST_URL = "https://occup-ie.herokuapp.com/api/v1";
 
     public static final String USER = HOST_URL + "/user";
-    public static final String LISTING = HOST_URL + "/listing";
+    public static final String RENTAL = HOST_URL + "/rental";
+    public static final String HOUSE_SHARE = HOST_URL + "/house-share";
     public static final String LANDLORD = HOST_URL + "/landlord";
-    public static final String APPLICATION = HOST_URL + "/listing";
+    public static final String APPLICATION = HOST_URL + "/application";
 }
