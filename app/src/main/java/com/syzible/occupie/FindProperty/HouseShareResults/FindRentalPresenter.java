adfde054965b8ff19a8ffab6160c874fc22a8b97
Edmond O' Flynn <oflynned@tcd.ie>
2018-04-02 00:22:14 +0100

    removed find property directory and renamed files

--- app/src/main/java/com/syzible/occupie/FindProperty/HouseShareResults/FindRentalPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.syzible.occupie.FindProperty.HouseShareResults.RentalResults;
-
-import com.syzible.occupie.Common.Mvp;
-
-public interface FindRentalPresenter extends Mvp.IPresenter<FindRentalView> {
-    void getProperties();
-}
