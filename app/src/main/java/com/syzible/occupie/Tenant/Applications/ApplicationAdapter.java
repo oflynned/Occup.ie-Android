7e5f067ad9f07f2b3d11e0835895172d0016f7ed
Edmond O' Flynn <oflynned@tcd.ie>
2018-04-14 18:49:14 +0100

    added applications adapter

--- app/src/main/java/com/syzible/occupie/Tenant/Applications/ApplicationAdapter.java
+++ app/src/main/java/com/syzible/occupie/Tenant/Applications/ApplicationAdapter.java
@@ -1,4 +1,43 @@
 package com.syzible.occupie.Tenant.Applications;
 
-public class ApplicationAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.syzible.occupie.Common.Objects.Application;
+import com.syzible.occupie.R;
+import com.syzible.occupie.Tenant.FindProperty.Results.RentalResults.PropertyResultsAdapter;
+
+import java.util.List;
+
+public class ApplicationAdapter extends RecyclerView.Adapter<ApplicationAdapter.ViewHolder> {
+
+    private List<Application> applications;
+
+    public ApplicationAdapter(List<Application> applications) {
+        this.applications = applications;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.property_tile, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return applications.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
