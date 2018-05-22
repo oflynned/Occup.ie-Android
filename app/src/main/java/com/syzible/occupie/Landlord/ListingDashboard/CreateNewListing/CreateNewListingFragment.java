7209682be1a4c9a4acbbc6ef5a4161666ace511b
Edmond O' Flynn <oflynned@tcd.ie>
2018-05-22 22:08:30 +0100

    added skeleton frag for creating new listings

--- app/src/main/java/com/syzible/occupie/Landlord/ListingDashboard/CreateNewListing/CreateNewListingFragment.java
+++ app/src/main/java/com/syzible/occupie/Landlord/ListingDashboard/CreateNewListing/CreateNewListingFragment.java
@@ -1,4 +1,24 @@
 package com.syzible.occupie.Landlord.ListingDashboard.CreateNewListing;
 
-public class CreateNewListingFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreateNewListingFragment extends Fragment {
+
+    public CreateNewListingFragment() {
+    }
+
+    public static CreateNewListingFragment getInstance() {
+        return new CreateNewListingFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
